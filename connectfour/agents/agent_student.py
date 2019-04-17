b21ec4d6eb8ad7a0737b1686ac2c9f08a97e387b
Ujjwal Batra <ujjwalbatra2509@gmail.com>
2019-04-17 19:05:00 +1000

    c4-contest-submission

--- connectfour/agents/agent_student.py
+++ connectfour/agents/agent_student.py
@@ -5,7 +5,7 @@ import random
 class StudentAgent(RandomAgent):
     def __init__(self, name):
         super().__init__(name)
-        self.MaxDepth = 7
+        self.MaxDepth = 6
 
     def get_move(self, board):
         """
@@ -32,7 +32,7 @@ class StudentAgent(RandomAgent):
     def dfMiniMax(self, board, alpha, beta, depth):
         # Goal return column with maximized scores of all possible next states
 
-        if depth == self.MaxDepth or board.terminal():
+        if depth == self.MaxDepth or board.winner() != 0:
             return self.evaluateBoardState(board)
 
         moves = []
@@ -113,13 +113,130 @@ class StudentAgent(RandomAgent):
 
         student_agent_utility = 0
         nemesis_utility = 0
+        nemesis_id = 1 if (self.id == 2) else 2
 
         for i in range(6):
             for col in range(7):
                 row = 5 - i
-                if board.get_cell_value(row, col) == 2:
-                    student_agent_utility += map_[row][col]
+                if board.get_cell_value(row, col) == self.id:
+                    student_agent_utility += self.__get_horizontal_utility(board, row, col, self.id) \
+                                             + self.__get_vertical_utility(board, row, col, self.id) \
+                                             + self.__get_diagonal_utility(board, row, col, self.id)
                 elif board.get_cell_value(row, col) == 1:
-                    nemesis_utility += map_[row][col]
+                    nemesis_utility += self.__get_horizontal_utility(board, row, col, nemesis_id) \
+                                       + self.__get_vertical_utility(board, row, col, nemesis_id) \
+                                       + self.__get_diagonal_utility(board, row, col, nemesis_id)
 
         return student_agent_utility - nemesis_utility
+
+    def __get_horizontal_utility(self, board, row, col, id):
+        start = col
+        end = col
+
+        # move start towards left
+        while start > 0 and start > col - 4:
+            previous_horizontal_dot = board.get_cell_value(row, start - 1)
+            if previous_horizontal_dot == 0 or previous_horizontal_dot == id:
+                start -= 1
+            else:
+                break
+
+        # move end towards right
+        while end < board.DEFAULT_WIDTH - 1 and end < col + 4:
+            next_horizontal_dot = board.get_cell_value(row, end + 1)
+            if next_horizontal_dot == 0 or next_horizontal_dot == id:
+                end += 1
+            else:
+                break
+
+        if (end - start) < 3:
+            return 0
+        else:
+            return (end - start) - 2
+
+    def __get_vertical_utility(self, board, row, col, id):
+        start = row
+        end = row
+
+        # move start towards bottom
+        while start < board.DEFAULT_HEIGHT - 1 and start < row + 4:
+            next_vertical_dot = board.get_cell_value(start + 1, col)
+            if next_vertical_dot == 0 or next_vertical_dot == id:
+                start += 1
+            else:
+                break
+
+        # move end towards top
+        while end > 0 and end > row - 4:
+            previous_vertical_dot = board.get_cell_value(end - 1, col)
+            if previous_vertical_dot == 0 or previous_vertical_dot == id:
+                end -= 1
+            else:
+                break
+
+        if (start - end) < 3:
+            return 0
+        else:
+            return (start - end) - 2
+
+    def __get_diagonal_utility(self, board, row, col, id):
+        return self.__get_primary_diagonal_utility(board, row, col, id) \
+               + self.__get_non_primary_diagonal_utility(board, row, col, id)
+
+    def __get_primary_diagonal_utility(self, board, row, col, id):
+        start = [row, col]
+        end = [row, col]
+
+        # move start towards top left corner
+        while start[0] > 0 and start[1] > 0 \
+                and start[0] > row - 4 and start[1] > col - 4:
+            previous_diagonal_dot = board.get_cell_value(start[0] - 1, start[1] - 1)
+            if previous_diagonal_dot == 0 or previous_diagonal_dot == id:
+                start[0] -= 1
+                start[1] -= 1
+            else:
+                break
+
+        # move end towards bottom right corner
+        while (end[0] < board.DEFAULT_HEIGHT - 1) and (end[1] < board.DEFAULT_WIDTH - 1) \
+                and (end[0] < row + 4) and (end[1] < col + 4):
+            next_diagonal_dot = board.get_cell_value(end[0] + 1, end[1] + 1)
+            if next_diagonal_dot == 0 or next_diagonal_dot == id:
+                end[0] += 1
+                end[1] += 1
+            else:
+                break
+
+        if (end[0] - start[0]) < 3:
+            return 0
+        else:
+            return (end[0] - start[0]) - 2
+
+    def __get_non_primary_diagonal_utility(self, board, row, col, id):
+        start = [row, col]
+        end = [row, col]
+
+        # move start towards bottom left corner
+        while (start[0] < board.DEFAULT_HEIGHT - 1) and (start[1] > 0) \
+                and (start[0] < row + 4) and (start[1] > col - 4):
+            previous_diagonal_dot = board.get_cell_value(start[0] + 1, start[1] - 1)
+            if previous_diagonal_dot == 0 or previous_diagonal_dot == id:
+                start[0] += 1
+                start[1] -= 1
+            else:
+                break
+
+        # move end towards top right corner
+        while (end[0] > 0) and (end[1] < board.DEFAULT_WIDTH - 1) \
+                and (end[0] > row - 4) and (end[1] < col + 4):
+            next_diagonal_dot = board.get_cell_value(end[0] - 1, end[1] + 1)
+            if next_diagonal_dot == 0 or next_diagonal_dot == id:
+                end[0] -= 1
+                end[1] += 1
+            else:
+                break
+
+        if (end[1] - start[1]) < 3:
+            return 0
+        else:
+            return (end[1] - start[1]) - 2
